56f8ca1e80e49dc8c1ebaf755392df076ec6fb09
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2020-11-23 18:28:07 +0100

    Añadido script

---
+++ balanceador.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Variables
+IP_PRIVADA_FRONT1=172.31.22.108
+IP_PRIVADA_FRONT2=172.31.32.188
+
+# Activar depuracion
+set -x
+
+# Instalar apache
+apt install apache2 -y
+
+#Habilitar modulos
+a2enmod proxy
+a2enmod proxy_http
+a2enmod proxy_ajp
+a2enmod rewrite
+a2enmod deflate
+a2enmod headers
+a2enmod proxy_balancer
+a2enmod proxy_connect
+a2enmod proxy_html
+a2enmod lbmethod_byrequests
+
+# Copiar 000-default
+cp /home/ubuntu/iaw-practica-03/000-default.conf /etc/apache2/sites-available/
+
+# Reemplazar variables
+sed -i "s/IP-HTTP-SERVER-1/$IP_PRIVADA_FRONT1/" /etc/apache2/sites-available/000-default.conf
+sed -i "s/IP-HTTP-SERVER-2/$IP_PRIVADA_FRONT2/" /etc/apache2/sites-available/000-default.conf
+
+# Reiniciar apache
+systemctl restart apache2
